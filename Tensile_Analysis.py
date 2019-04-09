95edf5f019df200de68cfaef21b6ba3842b18a92
tmorris <morris.trey.j@gmail.com>
2019-04-08 20:19:24 -0400

    Initial Commit of Tensile Analysis and initial try for automated toe-compensation.

---
+++ Tensile_Analysis.py
@@ -0,0 +1,124 @@
+import pandas as pd
+import re
+import csv
+import matplotlib.pyplot as plt
+import os
+import seaborn as sns
+
+'''set cross-sectional area'''
+area = 19.2 #cm^2
+'''gauge length'''
+g_len = 25 #mm
+
+'''get file locations of the raw .dat file'''
+file = '/Users/ICCAE1/Documents/programming/stress_strain/data/Specimen_RawData_1.csv'
+
+'''get file name to write output file'''
+file_name = os.path.splitext(file)[0]
+file_name = os.path.basename(file_name)
+print(file_name)
+
+'''open file as a giant text blob'''
+text = open(file, 'r+')
+
+'''seperate blob into lines of text'''
+lines = text.readlines()
+
+'''create list object to put in only numerical data'''
+clean_data = []
+
+'''go line by line to scrub header data'''
+for line in lines:
+
+    '''convert all \t to an actual tab in the data'''
+    line = line.replace('\t', ',')
+    #print(line)
+
+    '''strip out the \n at the end of each line'''
+    line = line.rstrip()
+
+    '''check to see if there are any letters in the string. If there are not it must be a data line'''
+    if re.search('[a-zA-Z]', line) == None:
+
+        '''if there is actually values in the line then we add it to our cleaned data list object'''
+        if len(line) > 4:
+
+            '''split the line into multiple elements using the comma as the anchor point'''
+            line = line.split(',')
+
+            '''add this line to the list object for ingest into Pandas'''
+            clean_data.append(line)
+
+'''create list of column titles'''
+#col_name = ['Force', 'Displacement', 'Time', 'Count']
+col_name = ['Time', 'Extension', 'Load']
+
+'''create pandas dataframe with the cleaned data'''
+data = pd.DataFrame(clean_data, columns=col_name)
+
+
+
+'''change the data from being a string to a float datatype'''
+data = data.astype(float)
+
+'''print out specfic coloumns of the dataframe'''
+#print(data['Force'])
+#print(data['Displacement'])
+#print(data['Time'])
+#print(data['Count'])
+
+'''try to take out the data after the specimen has broken'''
+data = data[0:-75]
+
+#data.to_excel(file_name+'.xls')
+data['difference'] = data['Load'].diff()
+
+'''trim front part of data'''
+data = data[100:-1]
+
+'''defining windowlength'''
+windowlength = 50
+
+'''smoothing data with a window of windowlength'''
+data['smooth'] = data['difference'].rolling(windowlength).mean()
+
+'''finding initial maximum of trimmed and smoothed data'''
+minrange = data['smooth'].idxmax()
+
+'''corrected minrange value based on windowlength'''
+correctedminrange = minrange - (3 * windowlength)
+
+'''taking the difference of the difference'''
+data['differencetoo'] = data['difference'].diff()
+
+'''plot differencetoo vs Extension'''
+data.plot(y='differencetoo', x='Extension')
+
+'''smoothing differencetoo data with a window of 1000'''
+data['smoothtoo'] = data['differencetoo'].rolling(1000).mean()
+
+'''plot smoothtoo vs Extension'''
+data.plot(y='smoothtoo', x='Extension')
+
+'''finding initial minimum of trimmed and smoothedtoo data'''
+maxrange = data['smoothtoo'].idxmin()
+
+'''plot Slope'''
+#data.plot(y='difference', x='Extension')
+
+'''plot Stress vs. Strain'''
+#data.plot(y='Load', x='Extension')
+
+'''plot Smooth vs. Time'''
+#data.plot(y='smooth', x='Extension')
+
+'''plot Stress vs. Strain'''
+#data.plot(y='Load', x='Extension')
+
+'''defining ax variable to be corrrected midrange vs maxrange'''
+ax = data.iloc[correctedminrange:maxrange].plot(y='Load', x='Extension')
+
+'''plotting trendline for corrected load vs Extension'''
+sns.lmplot(y='Load', x='Extension', data = data[correctedminrange:maxrange],fit_reg=True)
+
+plt.show()
