2622c39a8e9989865b3e6ac46a9720d976139eff
Ariloo <millerarielle15@gmail.com>
2019-04-23 05:49:37 -0400

    Updated the data cleaning to focus primarily on fatigue data, pulling all files from path and running in a loop then sending some data to an output file and the bulk of the raw data with normalization to an xlxs file.

--- dat_file_cleaning.py
+++ dat_file_cleaning.py
@@ -3,93 +3,243 @@ import re
 import csv
 import matplotlib.pyplot as plt
 import os
+import openpyxl
+from openpyxl import Workbook, load_workbook
 
-'''set cross-sectional area'''
-area = 41.6 #cm^2
-'''gauge length'''
-g_len = 50 #mm
+Fatigue = 'File Name  Applied Stress  Max Cycles \n'
 '''get file locations of the raw .dat file'''
-file = 'data/low_high_4545_typei_monotonic_r06.csv'
+for i in range(0,2):
+    layer_height = ['0.3302', '0.254']
+    infill_density = ['solid', 'hd']
+    type = ['typei', 'd3039']
+    file_front = 'data/' + str(layer_height[i]) + '_' + str(infill_density[i])+'_4545_'+str(type[i])
+    for j in range (0,4):
+        uts_percent =['95%uts','85%uts', '75%uts', '65%uts']
+        file_back = file_front + '_' + str(uts_percent[j]) +'_0.25hz_r('
+        for replicates in range(1,4):
+            file = file_back+str(replicates)+').dat'
+            print(file)
+            split_filename = file.split('_')
+            specimen_type = split_filename[3]
+            test_type = split_filename[4]
+            layer = split_filename[0]
+            split_layer = layer.split('/')
+            layer_thickness = split_layer[1]
+            infill = split_filename[1]
+            frequency = split_filename[5]
+            percent_uts = split_filename[4]
 
-'''get file name to write output file'''
-file_name = os.path.splitext(file)[0]
-file_name = os.path.basename(file_name)
-print(file_name)
+            if specimen_type in ['typei']:
+                '''D638 type i cross-sectional area'''
+                area = 41.6  # cm^2
+                '''gauge length'''
+                g_len = 50  # mm
 
-'''open file as a giant text blob'''
-text = open(file, 'r+')
+            elif specimen_type in ['typeiv']:
+                '''D638 type iv cross-sectional area'''
+                area = 19.8  # cm^2
+                '''gauge length'''
+                g_len = 25  # mm
+            else:
+                '''D3039 cross-sectional area'''
+                area = 82.5 # cm^2
+                '''D3039 gauge length'''
+                g_len = 180  # mm
 
-'''seperate blob into lines of text'''
-lines = text.readlines()
+            print('The '+str(specimen_type)+' cross-sectional area is', area, ' mm2')
+            print('The '+str(specimen_type)+' gage length is', g_len, ' mm')
+            '''get file name to write output file'''
+            file_name = os.path.splitext(file)[0]
+            file_name = os.path.basename(file_name)
+            print(file_name)
 
-'''create list object to put in only numerical data'''
-clean_data = []
+            '''open file as a giant text blob'''
+            if os.path.isfile(file):
+                # ...
+                with open(file, 'r+') as text:
+                    pass
+                    '''seperate blob into lines of text'''
+                    lines = text.readlines()
 
-'''go line by line to scrub header data'''
-for line in lines:
+                    '''create list object to put in only numerical data'''
+                    clean_data = []
 
-    '''convert all \t to an actual tab in the data'''
-    line = line.replace('\t', ',')
-    #print(line)
+                    '''go line by line to scrub header data'''
+                    for line in lines:
 
-    '''strip out the \n at the end of each line'''
-    line = line.rstrip()
+                        '''convert all \t to an actual tab in the data'''
+                        line = line.replace('\t', ',')
+                        #print(line)
 
-    '''check to see if there are any letters in the string. If there are not it must be a data line'''
-    if re.search('[a-zA-Z]', line) == None:
+                        '''strip out the \n at the end of each line'''
+                        line = line.rstrip()
 
-        '''if there is actually values in the line then we add it to our cleaned data list object'''
-        if len(line) > 4:
+                        '''check to see if there are any letters in the string. If there are not it must be a data line'''
+                        if re.search('[a-zA-Z]', line) == None:
 
-            '''split the line into multiple elements using the comma as the anchor point'''
-            line = line.split(',')
+                            '''if there is actually values in the line then we add it to our cleaned data list object'''
+                            if len(line) > 4:
 
-            '''add this line to the list object for ingest into Pandas'''
-            clean_data.append(line)
+                                '''split the line into multiple elements using the comma as the anchor point'''
+                                line = line.split(',')
 
-'''create list of column titles'''
-col_name = ['Displacement', 'Force', 'Time']
+                                '''add this line to the list object for ingest into Pandas'''
+                                clean_data.append(line)
 
-'''create pandas dataframe with the cleaned data'''
-data = pd.DataFrame(clean_data, columns=col_name)
+                    '''create list of column titles'''
+                    if test_type in ['monotonic']:
+                        #tensile test data columns
+                        col_name = ['Displacement', 'Force', 'Time']
+                    else:
+                        #fatigue test data columns
+                        col_name = ['Force', 'Displacement', 'Time', 'Count']
 
-'''change the data from being a string to a float datatype'''
-data = data.astype(float)
+                    '''create pandas dataframe with the cleaned data'''
+                    data = pd.DataFrame(clean_data, columns=col_name)
+                    print(data.head())
 
-'''print out specfic coloumns of the dataframe'''
-#print(data['Force'])
-#print(data['Displacement'])
-#print(data['Time'])
-#print(data['Count'])
+                    '''change the data from being a string to a float datatype'''
+                    data = data.astype(float)
 
+                    '''Stress calculation'''
+                    data['Stress'] = data['Force'] / area
 
-max = data[5000:6000].max()
-print(max['Force'])
-min = data[5000:6000].min()
-print(min['Force'])
+                    '''Normalize the Displacement'''
+                    data['NormalDisplacement'] = data['Displacement']-data.iloc[0]['Displacement']
 
-'''take subset of data where the force value is greater than zero'''
-#data = data.loc[(data['Force'] > min['Force']) & (data['Force'] < max['Force'])]
+                    '''Strain Calculation'''
+                    data['Strain'] = data['NormalDisplacement'] / g_len
+                    print(data['Strain'])
+                    print(data['Stress'])
 
-'''try to take out the data after the specimen has broken'''
-#data = data[0:-75]
+                    '''calculating applied stress'''
 
-'''Stress calculation'''
-data['Stress'] = data['Force'] / area
+                    for specimen_type in ['typei']:
+                        for frequency in ['0.25hz']:
+                            for layer_thickness in ['0.254']:
+                                for infill in ['solid']:
+                                    typei_uts = 29.18 #MPa
+                                    if percent_uts in ['95%uts']:
+                                        applied_stress = 0.95*typei_uts
+                                    elif percent_uts in ['85%uts']:
+                                        applied_stress = 0.85*typei_uts
+                                    elif percent_uts in ['75%uts']:
+                                        applied_stress = 0.75*typei_uts
+                                    else:
+                                        applied_stress = 0.65*typei_uts
+                                    #calculate the maximum number of cycles and applied stress for fatigue data'''
+                                    max_cycles = data['Count'].max()
+                    for specimen_type in ['typei']:
+                        for frequency in ['0.25hz']:
+                            for layer_thickness in ['0.3302']:
+                                for infill in ['solid']:
+                                    typei_uts = 29.18 #MPa
+                                    if percent_uts in ['95%uts']:
+                                        applied_stress = 0.95 * typei_uts
+                                    elif percent_uts in ['85%uts']:
+                                        applied_stress = 0.85 * typei_uts
+                                    elif percent_uts in ['75%uts']:
+                                        applied_stress = 0.75 * typei_uts
+                                    else:
+                                        applied_stress = 0.65 * typei_uts
+                                    #calculate the maximum number of cycles and applied stress for fatigue data'''
+                                    max_cycles = data['Count'].max()
+                    for specimen_type in ['typei']:
+                        for frequency in ['0.25hz']:
+                            for layer_thickness in ['0.254']:
+                                for infill in ['hd']:
+                                    typei_uts = 24.63 #MPa
+                                    if percent_uts in ['95%uts']:
+                                        applied_stress = 0.95 * typei_uts
+                                    elif percent_uts in ['85%uts']:
+                                        applied_stress = 0.85 * typei_uts
+                                    elif percent_uts in ['75%uts']:
+                                        applied_stress = 0.75 * typei_uts
+                                    else:
+                                        applied_stress = 0.65 * typei_uts
+                                    # calculate the maximum number of cycles and applied stress for fatigue data
+                                    max_cycles = data['Count'].max()
+                    for specimen_type in ['typei']:
+                        for frequency in ['0.25hz']:
+                            for layer_thickness in ['0.3302']:
+                                for infill in ['hd']:
+                                    typei_uts = 24.2 #MPa
+                                    if percent_uts in ['95%uts']:
+                                        applied_stress = 0.95 * typei_uts
+                                    elif percent_uts in ['85%uts']:
+                                        applied_stress = 0.85 * typei_uts
+                                    elif percent_uts in ['75%uts']:
+                                        applied_stress = 0.75 * typei_uts
+                                    else:
+                                        applied_stress = 0.65 * typei_uts
+                                    # calculate the maximum number of cycles and applied stress for fatigue data
+                                    max_cycles = data['Count'].max()
 
-'''Normalize the Displacement'''
-data['NormalDisplacement']= data['Displacement']-data.iloc[0]['Displacement']
+                    for specimen_type in ['d3039']:
+                        for frequency in ['0.25hz']:
+                            for layer_thickness in ['0.254']:
+                                for infill in ['solid']:
+                                    typei_uts = 26.09 #MPa
+                                    if percent_uts in ['95%uts']:
+                                        applied_stress = 0.95 * typei_uts
+                                    elif percent_uts in ['85%uts']:
+                                        applied_stress = 0.85 * typei_uts
+                                    elif percent_uts in ['75%uts']:
+                                        applied_stress = 0.75 * typei_uts
+                                    else:
+                                        applied_stress = 0.65 * typei_uts
+                                    # calculate the maximum number of cycles and applied stress for fatigue data
+                                    max_cycles = data['Count'].max()
+                    for specimen_type in ['d3039']:
+                        for frequency in ['0.25hz']:
+                            for layer_thickness in ['0.3302']:
+                                for infill in ['solid']:
+                                    typei_uts = 25.37 #MPa
+                                    if percent_uts in ['95%uts']:
+                                        applied_stress = 0.95 * typei_uts
+                                    elif percent_uts in ['85%uts']:
+                                        applied_stress = 0.85 * typei_uts
+                                    elif percent_uts in ['75%uts']:
+                                        applied_stress = 0.75 * typei_uts
+                                    else:
+                                        applied_stress = 0.65 * typei_uts
+                                    # calculate the maximum number of cycles and applied stress for fatigue data
+                                    max_cycles = data['Count'].max()
+                    for specimen_type in ['d3039']:
+                        for frequency in ['0.25hz']:
+                            for layer_thickness in ['0.254']:
+                                for infill in ['hd']:
+                                    typei_uts = 23.55 #MPa
+                                    if percent_uts in ['95%uts']:
+                                        applied_stress = 0.95 * typei_uts
+                                    elif percent_uts in ['85%uts']:
+                                        applied_stress = 0.85 * typei_uts
+                                    elif percent_uts in ['75%uts']:
+                                        applied_stress = 0.75 * typei_uts
+                                    else:
+                                        applied_stress = 0.65 * typei_uts
+                                    # calculate the maximum number of cycles and applied stress for fatigue data'''
+                                    max_cycles = data['Count'].max()
+                    for specimen_type in ['d3039']:
+                        for frequency in ['0.25hz']:
+                            for layer_thickness in ['0.3302']:
+                                for infill in ['hd']:
+                                    typei_uts = 21.25 #MPa
+                                    if percent_uts in ['95%uts']:
+                                        applied_stress = 0.95 * typei_uts
+                                    elif percent_uts in ['85%uts']:
+                                        applied_stress = 0.85 * typei_uts
+                                    elif percent_uts in ['75%uts']:
+                                        applied_stress = 0.75 * typei_uts
+                                    else:
+                                        applied_stress = 0.65 * typei_uts
+                                    # calculate the maximum number of cycles and applied stress for fatigue data'''
+                                    max_cycles = data['Count'].max()
+                    Fatigue = Fatigue + file + ' ' + str(applied_stress) + ' ' + str(max_cycles) + '\n'
 
-'''Strain Calculation'''
-data['Strain'] = data['NormalDisplacement'] / g_len
-#print(data['Strain'])
+            True
 
-'''plot Force vs. Displacement'''
-data.plot(y='Force', x = 'Displacement')
-
-'''plot Stress vs. Strain'''
-data.plot(y='Stress', x='Strain')
-
-#plt.show()
-
-data.to_excel('data/'+file_name+'.xls')
+print(Fatigue)
+a1 = open('output', 'w')
+a1.writelines(Fatigue)
+#    data.to_excel('data/'+file_name+'.xlsx')
